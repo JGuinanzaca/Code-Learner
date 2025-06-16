d4de4d5ba943eed62db81ccbb0a3abaa81c4da4a
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-06-16 14:24:23 -0400

    removed lines related to db.js for testing purposes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const userRoutes = require("./userRoutes.js");
-const connectDatabase = require("./db.js");
+//const connectDatabase = require("./db.js");
 const server = express();
 const port = 3000;
 
-connectDatabase(); //from db.js
+//connectDatabase(); //from db.js
 server.use("/codelearner", userRoutes); // Route defined in userRoutes.js
 
 server.listen(port, () => {
