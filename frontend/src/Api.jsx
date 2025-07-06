09f14d6ac696a4ecb644b6366bc3b96126066856
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-07-06 13:08:37 -0400

    renamed Auth to Api (idk lol), added fetchUsers and fetchLessons by id

---
+++ frontend/src/Api.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:5000";
+
+export const register = async (userData) => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/codelearner/register`,
+      userData
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const login = async (userData) => {
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/codelearner/login`,
+      userData
+    );
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const fetchUsers = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/codelearner/users/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export const fetchLessons = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/codelearner/lessons/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
