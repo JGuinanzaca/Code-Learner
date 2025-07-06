e5247714c78f1a078ac7f603ef92bc6207554d81
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-07-06 14:57:58 -0400

    added react-redux and @reduxjs/toolkit to frontend dependency; created authSlice that stores the user's id to be used across all components in the app if needed. index.js is already configured with the provider and store so id can be accessed throughout the app. only issue is at least on personal testing, api is being called twice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,50 +1,52 @@
-import React from "react"; 
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { ThemeProvider, useTheme } from './pages/ThemeProvider.jsx';
+import { ThemeProvider, useTheme } from "./pages/ThemeProvider.jsx";
 
-import Header from './pages/Header.jsx';
-import Footer from './pages/Footer.jsx';
-import HomePage from './App.jsx';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import Header from "./pages/Header.jsx";
+import Footer from "./pages/Footer.jsx";
+import HomePage from "./App.jsx";
 import Forums from "./pages/Forums.jsx";
-import Code from "./pages/Practice.jsx"
+import Code from "./pages/Practice.jsx";
 import Docs from "./pages/Docs.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignUp from "./pages/Signup.jsx";
 
 function App() {
-
   const { theme, toggleTheme } = useTheme();
-  const darkMode = theme !== 'dark';
+  const darkMode = theme !== "dark";
 
   return (
-      <>
-        <Header 
-          darkMode={darkMode} 
-          toggleTheme={toggleTheme} />
-        <main>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/code' element={<Code />} />
-            <Route path='/forums' element={<Forums />} />
-            <Route path='/docs' element={<Docs />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUp />} />
-          </Routes>
-          <Footer />
-        </main>
-      </>
+    <>
+      <Header darkMode={darkMode} toggleTheme={toggleTheme} />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/code" element={<Code />} />
+          <Route path="/forums" element={<Forums />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </main>
+    </>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </React.StrictMode>
+  </Provider>
 );
