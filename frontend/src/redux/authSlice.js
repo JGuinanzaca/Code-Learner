b11bc8800e9d03028911c413cccbdabd79e8fe9b
Courage_2001 <JGuinanzaca2001@gmail.com>
2025-07-11 18:30:26 -0400

    dummy commit

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -16,12 +16,3 @@ export const { saveId } = authSlice.actions;
 export const selectId = (state) => state.auth.Id;
 
 export default authSlice.reducer;
-
-// index.js is already configured to the provider and the store, all that's needed is to
-// import the following functions and reducers:
-//      import { useDispatch, useSelector } from "react-redux";
-//      import { selectId, saveId } from "./redux/authSlice";
-// then you would save the id by invoking dispatch(saveId(whatever)) where dispatch = useDispatch()
-// and call the id in the store by using e.g., const id = useSelector((state) => selectId(state));
-
-// to access the data of promise after api call, use .then e.g. const result = login(data).then
