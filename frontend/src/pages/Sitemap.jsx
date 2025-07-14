bd86a7f0e1d9f18b8fe24a6b983227d0cb02d1f0
JGuinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-07-14 11:31:07 -0400

    sitemap now has authentication enabled

--- frontend/src/pages/Sitemap.jsx
+++ frontend/src/pages/Sitemap.jsx
@@ -1,28 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { selectId } from "../redux/authSlice";
+import { useSelector } from "react-redux";
 
 const sitemapLinks = [
-    { path: '/', label: 'Home' },
-    { path: '/code', label: 'Practice Coding' },
-    { path: '/forums', label: 'Forums' },
-    { path: '/docs', label: 'Read through code docs' },
-    { path: '/login', label: 'Login' },
-    { path: '/signup', label: 'Register' },
-    { path: '/sitemap', label: 'Sitemap' },
+  { path: "/", label: "Home" },
+  { path: "/code", label: "Practice Coding" },
+  { path: "/forums", label: "Forums" },
+  { path: "/docs", label: "Read through code docs" },
+  { path: "/login", label: "Login" },
+  { path: "/signup", label: "Register" },
+  { path: "/sitemap", label: "Sitemap" },
 ];
 
+// default value for id is -1, persistence is now guarenteed across the board
+// one issue is scrolling up and down on the window makes multiple calls (scrolling most likely causes refreshing)
+function NavToPath(path) {
+  const userId = useSelector((state) => selectId(state));
+  if (path === "/") return path;
+  if (userId !== -1) {
+    console.log("Authenticated"); // Debug
+    return path;
+  } else if (userId === -1) {
+    console.log("Not Authenticated"); // Debug
+    return "/login";
+  }
+}
+
 const Sitemap = () => (
-    <div style={{ padding: '2rem' }}>
-        <h1>Sitemap</h1>
-        <ul>
-            {sitemapLinks.map(link => (
-                <li key={link.path}>
-                    <Link to={link.path}>{link.label}</Link>
-                </li>
-            ))}
-        </ul>
-    </div>
+  <div style={{ padding: "2rem" }}>
+    <h1>Sitemap</h1>
+    <ul>
+      {sitemapLinks.map((link) => (
+        <li key={link.path}>
+          <Link to={NavToPath(link.path)}>{link.label}</Link>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default Sitemap;
